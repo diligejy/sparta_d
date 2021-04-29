5e5f2719d38fbbc4e3c578945123e77df136d397
diligejy <sjy049@gmail.com>
2021-04-30 00:25:09 +0900

    sparta 2주차 과제 7-2

--- homework/week02/js/07_2_input_val.js
+++ homework/week02/js/07_2_input_val.js
@@ -1,17 +1,18 @@
-let inputValue = document.querySelector('#input-value');
-let emptyValid = document.querySelector('.emptyValid');
-let box = document.querySelector('.question-box');
-let li = document.createElement('li');
+// 1. input-email 값을 가져온다.
+// 2. 만약 가져온 값에 @가 있으면 (includes 이용하기 - 찾아보자!)
+// 3. contact@gmail.com -> gmail 만 추출해서
+// 4. alert(도메인 값);으로 띄우기
+// 5. 만약 이메일이 아니면 '이메일이 아닙니다.' 라는 얼럿 띄우기
 
+let inputEmail = document.getElementById('input-email');
+let clickBtn = document.getElementById('click');
 
-emptyValid.addEventListener('click', () => {
-        // 1. input-value의 입력값을 가져온다.
-    // 2. 만약 입력값이 빈칸이면 if(입력값=='')
-    // 3. alert(입력값) 띄우기
-
-    if (inputValue.value.trim() === ''){
-        alert('빈값입니다');
+clickBtn.addEventListener('click', () => {
+    if (inputEmail.value.includes('@')){
+        email_domain = inputEmail.value.split('@')[1].split('.')[0]
+        alert(email_domain);
     } else {
-        li.textContent = `${inputValue.value.trim()}`;
+        alert('이메일이 아닙니다');
     }
-})
+}, false);
+
