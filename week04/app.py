edefb8b5894b2744385ed268058dd84ed8e98247
diligejy <sjy049@gmail.com>
2021-05-08 14:48:00 +0900

    front - flask connect & scraping & mongodb add

---
+++ week04/app.py
@@ -0,0 +1,96 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+app = Flask(__name__)
+
+# MongoDB 추가
+client = MongoClient('localhost', 27017)
+db = client.get_database('sparta')
+
+
+@app.route('/')
+def home():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('index.html')
+
+
+@app.route('/book')
+def book_page():
+    return render_template('memo.html')
+
+
+@app.route('/memo', methods=['POST'])
+def memo_post():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    response = requests.get(
+        url_receive,
+        headers=headers
+    )
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.select_one('meta[property="og:title"]')
+    url = soup.select_one('meta[property="og:url"]')
+    image = soup.select_one('meta[property="og:image"]')
+    description = soup.select_one('meta[property="og:description"]')
+    print('제목 : ', title['content'])
+    print('URL : ', url['content'])
+    print('이미지 : ', image['content'])
+    print('상세설명 : ',  description['content'])
+    print('---')
+
+    document = {
+        'title': title['content'],
+        'url': url['content'],
+        'image': image['content'],
+        'description': description['content'],
+        'comment': comment_receive,
+    }
+    db.articles.insert_one(document)
+    return jsonify({'result': 'success', 'msg': '저장했읍니다'})
+
+
+@ app.route('/memo', methods=['GET'])
+def memo_get():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+    return jsonify()
+
+
+@ app.route('/review', methods=['POST'])
+def write_review():
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
+    # DB에 삽입할 review 만들기
+    review = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
+    # reviews에 review 저장하기
+    db.reviews.insert_one(review)
+    # 성공 여부 & 성공 메시지 반환
+    return jsonify({'result': 'success', 'msg': '리뷰가 성공적으로 작성되었습니다.'})
+
+# API
+
+
+@ app.route('/test', methods=['GET'])
+def test_get():
+    title_receive = request.args.get('title_give')
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+
+@ app.route('/test', methods=['POST'])
+def test_post():
+    title_receive = request.args.get('title_give')
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
--- week04/week4.md
+++ week04/week4.md
@@ -10,3 +10,22 @@
     Poetry add -> poetry.lock에 설치된 내역 생성됨
     - poetry add flask
     - poetry add pymongo
+
+## Flask
+
+    1. 기본 폴더 구조 만들기
+    Flask 를 사용할 때는 기본 설정을 지켜주어야 합니다.
+    프로젝트 폴더 안에, 폴더를 만들어주세요!
+
+    static 폴더 : 이미지, css파일을 넣어둡니다.
+    templates 폴더 : html파일을 넣어둡니다.
+    app.py 파일
+
+## postman
+
+    - https://www.postman.com/
+    - 설치해야함
+
+    1. POST SEND하고 싶으면
+        - BODY에서 x-www-form-urlencoded에 KEY, VALUE 넣어서 SEND
+
