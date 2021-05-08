2180fd2453c35288fb4f9e788a2e1100a49c2da4
diligejy <sjy049@gmail.com>
2021-05-08 16:30:01 +0900

    add POST method

--- homework/week04/app.py
+++ homework/week04/app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def landing_page():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 
 @app.route('/view_memo')
 def view_memo_page():
     return render_template('memo.html')
+
+
+@app.route('/post_url_comment', methods=['POST'])
+def post_url_comment():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+    print('URL : ', url_receive)
+    print('Comment : ', comment_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
 
 
 if __name__ == '__main__':
