4282a2fb356c0130471ba7ea5fd1b92bce7b72d3
diligejy <sjy049@gmail.com>
2021-05-08 16:35:48 +0900

    poetry add requests, bs4

--- homework/week04/app.py
+++ homework/week04/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, jsonify
-
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.week04  # 'dbsparta'라는 이름의 db를 만듭니다.
-
 
 app = Flask(__name__)
 
@@ -25,8 +25,15 @@ def view_memo_page():
 def post_url_comment():
     url_receive = request.form['url_give']
     comment_receive = request.form['comment_give']
-    print('URL : ', url_receive)
-    print('Comment : ', comment_receive)
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    response = requests.get(
+        url_receive,
+        headers=headers
+    )
+    print(response.text)
+    print('-----' * 10)
     return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
 
 
