8c21e27be6cde4b1db024d82bc1d6608ea45d940
diligejy <sjy049@gmail.com>
2021-05-12 20:03:02 +0900

    app에 models등록 & db init migrate upgrade

--- homework/week04/sparta/models.py
+++ homework/week04/sparta/models.py
@@ -1,11 +1,8 @@
-from pybo import db
+from sparta import db
 
 class Document(db.Model):
-
-# 언제 포스팅이 된건지 create_date Column도 있어야 하지 않을까?
-# 그걸 만드려면 어떻게 해야 할까?
-
     id = db.Column(db.Integer, primary_key=True)
+    create_date = db.Column(db.DateTime(), nullable=False)
     title = db.Column(db.String(200), nullable=False)
     url = db.Column(db.Text(), nullable=False)
     image = db.Column(db.Text(), nullable=False)
